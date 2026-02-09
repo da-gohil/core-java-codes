0ff11e61e7d2263b1862c16b7d12609b16147f74
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-02-09 15:21:03 -0500

    Create ArrayCreation.java

---
+++ Week 01/ArrayCreation.java
@@ -0,0 +1,11 @@
+public class ArrayCreation {
+ 
+    public static void main(String[] args){
+
+        int[] MyArray = new int[20];
+
+        for(int i=0; i<MyArray.length; i++){
+            System.out.println(MyArray[i]);
+        }
+    }
+}

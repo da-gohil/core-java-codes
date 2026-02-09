fb4a6c0b63303d7bd805ccb121fc488888432a71
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-02-09 14:46:26 -0500

    Create IOusingScanner.java

---
+++ Week 01/IOusingScanner.java
@@ -0,0 +1,28 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+class Solution {
+    public void printNumber(Scanner sc) {
+        if(sc == null) return;
+        try{
+            if(sc.hasNextInt()){
+                int number = sc.nextInt();
+                System.out.println(number);
+            }else{
+                System.err.println("Input is not an integer");
+            }
+        }catch(NoSuchElementException | IllegalStateException e){
+                System.err.println("Error reading Input " + e);
+        }
+    }
+}
+
+class IOusingScanner{
+    public static void main(String [] args){
+        Solution sol = new Solution();
+    
+        try(Scanner scan = new Scanner(System.in)){
+            sol.printNumber(scan);
+        }
+    }
+}

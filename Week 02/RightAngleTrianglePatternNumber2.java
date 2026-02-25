7e1a6e2fe2a51ac76b5c98b273f5adb002892981
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-02-25 13:12:26 -0500

    Create RightAngleTrianglePatternNumber2.java

---
+++ Week 02/RightAngleTrianglePatternNumber2.java
@@ -0,0 +1,22 @@
+// Pattern Structure should look like this
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+
+
+public class RightAngleTrianglePatternNumber2{
+
+    public static void main(String [] args){
+        int n = Integer.parseInt(args[0]);
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}

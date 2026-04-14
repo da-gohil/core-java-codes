ec291de5c944d0a61fd1d7fdeb3a71429ae12112
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2026-04-14 12:23:41 -0400

    Create RotateMatrix.java

---
+++ Week 03 Arrays/Medium/RotateMatrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        //count the length of the matrix
+        int n = matrix.length;
+
+        //Transpose
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                //swap elements of (i,j) to (j,i)
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //Reverse each row
+        for(int i=0; i<n; i++){
+            int left = 0, right = n-1;
+
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
